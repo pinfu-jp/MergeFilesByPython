79faff6cac77b48d185bef524f0d85751f17a088
h.a <hidetaka.asahi.3@gmail.com>
2023-02-18 10:13:26 +0900

    引数でパスを指定できるように対応

--- src/main.py
+++ src/main.py
@@ -1,3 +1,4 @@
+import sys
 
 from src.parse_logs_to_csv import parse_logs_to_csv
 from src.select_folder import select_folder
@@ -6,6 +7,8 @@ from src.messagebox import show_message
 from src.logger import write_log, LogLevel
 
 
+# python main.py C:\Users\username\log_folder C:\Users\username\csv_folder 
+
 
 # メイン処理
 def main():
@@ -13,19 +16,29 @@ def main():
 	try:
 		write_log("main() 開始")
 
-		# ユーザーにフォルダを選択してもらう
-		log_folder = select_folder()
-		if not log_folder:
-			show_message("ログフォルダが指定されませんでした")		
-			return
+		if len(sys.argv) > 1:
+			write_log("パラメータあり")
+			log_folder_path = sys.argv[1]
+			csv_folder_path = sys.argv[2]
 
-		out_dir = log_folder 
+		else:
+			write_log("パラメータなし")
+
+			# ユーザーにフォルダを選択してもらう
+			log_folder_path = select_folder()
+			if not log_folder_path:
+				show_message("ログフォルダが指定されませんでした")		
+				return
+
+			csv_folder_path = log_folder_path 
+
+		write_log("log_folder:" + log_folder_path + " csv_folder_path:" + csv_folder_path)
 
 		# 複数のログファイルを １本のcsvファイルにまとめる 
-		parse_logs_to_csv(log_folder, out_dir)
+		parse_logs_to_csv(log_folder_path, csv_folder_path)
 
 		# CSVファイルを開く
-		open_csv(out_dir)
+		open_csv(csv_folder_path)
 
 	except Exception as e:
 		write_log("例外発生 e:" + str(e), LogLevel.E)
