c117eb2e650a9472b25c9509a717621d8995f476
h.a <hidetaka.asahi.3@gmail.com>
2023-02-18 12:01:42 +0900

    リファクタリング

--- module/parse_logs_to_csv.py
+++ module/parse_logs_to_csv.py
@@ -3,14 +3,16 @@ import re
 import csv
 
 from datetime import datetime, timedelta
+from typing import Optional
 
 from module.logger import write_log, LogLevel, DEBUG_LOG_PATH
 
 OUT_SYMBLE_PRE_WORD = '●'
 OUT_CSV_NAME = OUT_SYMBLE_PRE_WORD + 'parsed_log'
 
-# 指定されたフォルダ内の全てのログファイルに対して、タイムスタンプとログ文字列を分離し、csvファイルに出力
+
 def parse_logs_to_csv(log_folder_path, csv_folder_path, go_back_day_count = 5):
+	"""指定されたフォルダ内の全てのログファイルに対して、タイムスタンプとログ文字列を分離し、csvファイルに出力"""
 
 	write_log("parse_logs_to_csv() start go_back_day_count:" + str(go_back_day_count))
 
@@ -21,8 +23,9 @@ def parse_logs_to_csv(log_folder_path, csv_folder_path, go_back_day_count = 5):
 		csv_path = make_csv_path(csv_folder_path, target_date)
 		parse_one_day_logs_to_csv(log_folder_path, target_date, csv_path)
 
-# 指定されたフォルダ内の全てのログファイルに対して、タイムスタンプとログ文字列を分離し、csvファイルに出力
+
 def parse_one_day_logs_to_csv(log_folder_path, target_date:datetime, csv_file_path):
+	"""指定されたフォルダ内の全てのログファイルに対して、タイムスタンプとログ文字列を分離し、csvファイルに出力"""
 
 	write_log("parse_logs_to_csv() start")
 
@@ -45,8 +48,8 @@ def parse_one_day_logs_to_csv(log_folder_path, target_date:datetime, csv_file_pa
 		write_log("parse_logs_to_csv() end success")
 
 
-#  ファイルからタイムスタンプと文字列を抽出
 def parse_timestamp_by_files(out_lines, file_path:str, target_date:datetime):
+	"""ファイルからタイムスタンプと文字列を抽出"""
 
 	write_log("parse_timestamp_by_files() file:" + file_path, LogLevel.D)
 
@@ -94,7 +97,7 @@ def parse_timestamp_by_files(out_lines, file_path:str, target_date:datetime):
 
 
 def parse_timestamp_in_file_name(file_name) -> datetime:
-	# ファイル名からyyyymmdd形式の日付を取り出す
+	"""ファイル名からyyyymmdd形式の日付を取り出す"""
 	match = re.search(r'\d{8}', file_name)
 	if match:
 		return datetime.strptime(match.group(), '%Y%m%d')
@@ -107,8 +110,9 @@ def parse_timestamp_in_file_name(file_name) -> datetime:
 # 日付と時刻の間は半角スペース
 TIMESTAMP_PATTERN = re.compile(r'\d{4}[-./]\d{2}[-./]\d{2} \d{2}:\d{2}:\d{2}|\d{2}[-./]\d{2}[-./]\d{2} \d{2}:\d{2}:\d{2}')
 
-# タイムスタンプの時刻とログ文字列を分離し、リストに格納  ファイル名も出力
 def parse_time(log_line, file_name) :
+	"""タイムスタンプの時刻とログ文字列を分離し、リストに格納  ファイル名も出力"""
+
 	match = TIMESTAMP_PATTERN.search(log_line)
 	if match:
 		timestamp_str = match.group()
@@ -117,7 +121,7 @@ def parse_time(log_line, file_name) :
 		# dateime型 
 		timestamp = datetime_by_text(timestamp_str)
 		# UTF8 文字列にする
-		log_string_utf8 = ecode_to_utf8(log_string)
+		log_string_utf8 = encode_to_utf8(log_string)
 
 		write_log("timestamp:" + timestamp.strftime("%Y-%m-%d %H:%M:%S") + ", log_string_utf8:" + log_string_utf8, LogLevel.D)
 		return [timestamp, file_name, log_string_utf8]
@@ -135,40 +139,36 @@ TIMESTAMP_PATTERN = re.compile(r'\d{4}[-./]\d{2}[-./]\d{2} \d{2}:\d{2}:\d{2}|\d{
 TIMESTAMP_TIME_PATTERN = re.compile(r'\d{2}:\d{2}:\d{2}')
 
 
-# タイムスタンプとログ文字列を分離し、リストに格納  ファイル名も出力
-def parse_timestamp(log_line, file_name, file_timestamp) :
+def parse_timestamp(log_line: str, file_name: str, file_timestamp: Optional[datetime]):
+	"""タイムスタンプとログ文字列を分離し、リストに格納"""
 
-	if file_timestamp:	# ファイル名に日付が付いているケース
+	# 引数チェック
+	if not log_line or not file_name:
+		return None
+
+	if file_timestamp:  # ファイル名に日付が付いているケース
 		match = TIMESTAMP_TIME_PATTERN.search(log_line)
-		if match:
-			time_str = match.group()
-			log_string = log_line.replace(time_str, "").strip()
-
-			# dateime型 
-			timestamp = combine_time_str_to_datetime(file_timestamp, time_str)
-			# UTF8 文字列にする
-			log_string_utf8 = ecode_to_utf8(log_string)
-		else:
+		if not match:
 			return None
 
-	else:	# ファイル名に日付は付いていないケース
+		timestamp = combine_time_str_to_datetime(file_timestamp, match.group())
+
+	else:  # ファイル名に日付は付いていないケース
 		match = TIMESTAMP_PATTERN.search(log_line)
-		if match:
-			timestamp_str = match.group()
-			log_string = log_line.replace(timestamp_str, "").strip()
-
-			# dateime型 
-			timestamp = datetime_by_text(timestamp_str)
-			# UTF8 文字列にする
-			log_string_utf8 = ecode_to_utf8(log_string)
-		else:
+		if not match:
 			return None
 
-	write_log("timestamp:" + timestamp.strftime("%Y-%m-%d %H:%M:%S") + ", log_string_utf8:" + log_string_utf8, LogLevel.D)
+		timestamp = datetime_by_text(match.group())
+
+	# UTF-8 文字列にする
+	log_string = log_line.replace(match.group(), "").strip()
+	log_string_utf8 = encode_to_utf8(log_string)
+
+	write_log(f"timestamp: {timestamp}, log_string_utf8: {log_string_utf8}", LogLevel.D)
 	return [timestamp, file_name, log_string_utf8]
 
-# タイムスタンプ文字列を datetime 型に変換
 def datetime_by_text(timestamp_str) -> datetime:
+	"""タイムスタンプ文字列を datetime 型に変換"""
 
 	try:
 		isYearShort = not timestamp_str[:4].isdigit()
@@ -188,34 +188,33 @@ def datetime_by_text(timestamp_str) -> datetime:
 		return None
 
 
-# タイムスタンプ文字列を datetime 型に変換
 def combine_time_str_to_datetime(datetime, time_str) -> datetime:
+	"""タイムスタンプ文字列を datetime 型に変換"""
 
 	try:
 		# datetime型に変換
 		tm = datetime.strptime(time_str, '%H:%M:%S').time()
 		return datetime.combine(datetime, tm)
-
 	except ValueError as e:
 		write_log("combine_time_str_to_datetime error:" + str(e), LogLevel.E)
 		return None
 
 
+def encode_to_utf8(some_string):
+	"""文字列をUTF8にエンコード"""
 
-
-# 文字列をUTF8にエンコード
-def ecode_to_utf8(some_string):
-	# SJISでエンコードされているかどうか判定
-	if isinstance(some_string, bytes) and some_string[:2] == b'\x82\xb1':
-		some_string = some_string.decode('shift_jis')
-		# UTF-8にエンコードしてからファイルに出力する
-		return some_string.encode('utf-8').decode('utf-8')
+	if isinstance(some_string, bytes):
+		try:
+			decoded_string = some_string.decode('utf-8')
+		except UnicodeDecodeError:
+			decoded_string = some_string.decode('shift_jis')
+		return decoded_string.encode('utf-8')
 
 	return some_string
 
 
-# 対象日付か
 def is_target_log(timestamp:datetime, target_date:datetime):
+	"""対象日付か判定"""
 
 	if (timestamp.year != target_date.year):
 		return False
@@ -228,6 +227,7 @@ def is_target_log(timestamp:datetime, target_date:datetime):
 
 	return True
 
-# CSVファイルパスを作成
 def make_csv_path(csv_folder_path:str, target_date:datetime):
+	"""CSVファイルパスを作成"""
 	return os.path.join(csv_folder_path, OUT_CSV_NAME + "_" + target_date.strftime('%Y%m%d') + ".csv")
+    # return os.path.join(csv_folder_path, f"{OUT_CSV_NAME}_{target_date.strftime('%Y%m%d')}.csv")
