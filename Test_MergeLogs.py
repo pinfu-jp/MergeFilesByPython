798fffa0853797234f1d27bac2ea251bb08e7d64
h.a <hidetaka.asahi.3@gmail.com>
2023-02-25 09:26:18 +0900

    テストデータの更新

--- Test_MergeLogs.py
+++ Test_MergeLogs.py
@@ -2,18 +2,14 @@ import unittest
 import os
 import shutil
 import random
-import time
 import threading
+from datetime import datetime, timedelta
 
 from module.__init__ import APP_NAME
 from module.merge_logs_by_json import merge_logs_by_json
 
 class TestMergeLogs(unittest.TestCase):
 	"""MergeLogs ユニットテスト"""
-
-	def test_sys_path(self):
-		import sys; print(sys.path)
-
 
 	def test_parse_logs_by_json(self):
 
@@ -22,19 +18,17 @@ class TestMergeLogs(unittest.TestCase):
 
 		json = f"./{APP_NAME}.json"
 
-		# parse_logs_by_json テスト
+		#------------------------
+		# テスト対象の関数
 		merge_logs_by_json(json)
-
-		# テスト後に作成したファイルを削除
-		# os.remove(test_log_file)
-		# os.remove(test_csv_file)
+		#------------------------
 
 
 	def __prepare_log_test(self, directory):
 		"""テスト前準備"""
 
 		if os.path.exists(directory):
-			shutil.rmtree(directory)
+			shutil.rmtree(directory)	# 古いテストデータは削除
 		os.mkdir(directory)
 
 		test_log_file = directory + '/' + 'test.log'
@@ -78,12 +72,18 @@ class TestMergeLogs(unittest.TestCase):
 	def __make_random_logs(self, directory, file_count = 20, line_count=2000):
 		"""ランダムな情報を持つログファイルを複数作成"""
 
+		now = datetime.now()
+
 		threads = []
 		for i in range(file_count):
+
+			target_date = now - timedelta(days=i)
+
 			# ファイル毎にスレッドを分けて実行
 			test_log_file = directory + '/' + f'test_ramdom_{i}.log'
 			thread = threading.Thread(target=self.__make_random_log,
 										args=(	test_log_file,
+												target_date,
 												line_count))
 			threads.append(thread)
 			thread.start()
@@ -93,27 +93,37 @@ class TestMergeLogs(unittest.TestCase):
 			thread.join()
 
 
-	def __make_random_log(self, filename, line_count):
+	def __make_random_log(self, filename, target_date, line_count):
 		"""ランダムな大量データが入ったログファイルを作成"""
 
-		if os.path.exists(filename):
-			os.remove(filename)
+		try:
+			if os.path.exists(filename):
+				os.remove(filename)
 
-		# ファイルに書き込む
-		with open(filename, "w") as f:
+			# ファイルに書き込む
+			with open(filename, "w") as f:
 
-			for i in range(line_count):
-				# 現在時刻を取得し、文字列に変換する
-				timestamp = time.strftime("%Y-%m-%d %H:%M:%S")
+				for i in range(line_count):
 
-				# ランダムな文字列を生成する
-				random_string = "".join(random.choices("abcdefghijklmnopqrstuvwxyz", k=200))
+					target_date = self.__make_random_seconds(target_date)
 
-				# ファイルに書き込む文字列を生成する
-				line = f"{timestamp} {random_string}\n"
+					# 現在時刻を取得し、文字列に変換する
+					timestamp = target_date.strftime("%Y-%m-%d %H:%M:%S")
 
-				f.write(line)
+					# ランダムな文字列を生成する
+					random_string = "".join(random.choices("abcdefghijklmnopqrstuvwxyz", k=300))
 
+					# ファイルに書き込む文字列を生成する
+					line = f"{timestamp} {random_string}\n"
+
+					f.write(line)
+
+		except Exception as e:
+			assert e is None, "例外発生" 
+
+	def __make_random_seconds(self, time:datetime):
+		random_seconds = random.randint(1, 10)
+		return time + timedelta(seconds=random_seconds)
 
 if __name__ == '__main__':
     unittest.main()
