0857c6022c938d1386fdc8fc7594bbb08d4aadde
h.a <hidetaka.asahi.3@gmail.com>
2023-03-09 22:10:27 +0900

    ビルド設定更新

--- setup.py
+++ setup.py
@@ -1,7 +1,11 @@
 import sys
 import os
+import shutil
+import zipfile
+
 from cx_Freeze import setup, Executable
 
+APP_NAME = "MergeLogs"
 
 # ビルドに含めるパッケージとモジュールを指定
 packages = ['module']
@@ -11,7 +15,7 @@ excludes = []
 # 実行ファイルの設定
 exe = Executable(
     script='main.py',  # 実行ファイルとなるスクリプト
-    targetName='MergeLogs.exe',  # 出力ファイル名
+    targetName=(APP_NAME + '.exe'),  # 出力ファイル名
     base=None  # コンソールアプリケーションとしてビルド
 )
 
@@ -37,8 +41,23 @@ setup(
         "All rights reserved."
     ),)
 
+# 設定ファイルをコピーしてくる
+json_name = APP_NAME + ".json"
+src_path = os.path.join(".", json_name)
+dest_path = os.path.join(".", "build", "exe.win-amd64-3.11", json_name)
+if os.path.exists(dest_path):
+    os.remove(dest_path)
+shutil.copy(src_path, dest_path)
+
+dest_path = os.path.join(".", "build", "exe.win-amd64-3.11")
 
 # 作成されたexe等の出力先を開く
 if sys.platform == 'win32':
-    out_dir = os.path.join(".", "build", "exe.win-amd64-3.11")
-    os.system('explorer "{}"'.format(os.path.abspath(out_dir)))
+    os.system('explorer "{}"'.format(os.path.abspath(dest_path)))
+
+# # ZIP化 
+# zip_path = os.path.join(".", "package", APP_NAME + ".zip")
+# if os.path.exists(zip_path):
+#     os.remove(zip_path)
+# with zipfile.ZipFile(zip_path, mode='w', compression=zipfile.ZIP_DEFLATED) as zipf:
+#     zipf.write(dest_path)
